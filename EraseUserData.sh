d61b8de02616569bf80572e10274a7cb7faaafde
poc-sm <135996455+poc-sm@users.noreply.github.com>
2023-06-20 18:27:29 +0200

    Update EraseUserData.sh
    typo fix

--- EraseUserData.sh
+++ EraseUserData.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-echo Ignore any errors. This step can take up to 10 minutes, please wait!
+echo "Ignore any errors. This step can take up to 10 minutes, please wait!"
 
 fastboot flash userdata flash/userdata.img
 fastboot erase metadata
 fastboot format metadata
 fastboot -w 
 
-echo "Ignore any errors. /userdata and /metadata wiped ! Factory reset complete. Closing this window in 60 secondes"
+echo "Factory reset complete. Closing this window in 60 secondes"
 sleep 60
 exit
